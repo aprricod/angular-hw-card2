2729674dd5ab9c6333f2745f316349ae79ac9678
Artem <aprricod@gmail.com>
2021-02-11 14:05:41 +0300

    complete tasks 1-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homework1-card2';
+  title: string = 'homework1-card2';
+  msg = 'First msg';
+  arrayClick = [];
+
+  red = '255,0,0';
+  yellow = '255,255,0';
+  green = '0,255,0';
+  lightblue = '0,255,255';
+  blue = '0,0,255';
+  violet = '255,0,255';
+
+  add() {
+    this.arrayClick.push('Hello!');
+  }
+
+  clean() {
+    this.arrayClick = [];
+  }
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Time is over');
+      this.msg = 'Secong msg';
+    }, 5000);
+  }
 }
